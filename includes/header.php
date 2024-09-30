511c3c77bafb98623fd41f3ca86e142d70b0a07f
Elise Geoffroy <elise.geoffroy03@gmail.com>
2024-09-30 11:20:37 +0200

    nav connection #2

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,30 @@
 <header>LilyBlog - Accueil
-    <nav>
-        <a class="navtab" href='./register.php'>
-            Inscription
-        </a>
-        <a class="navtab" href='./login.php'>
-            Connexion
-        </a>
-        <a class="navtab" href='./logout.php'>
-            Déconnexion
-        </a>
-    </nav>
+    <?php require_once $dir . '/includes/authentication.php' ?>
+
+    <?php if (is_array(isLogged($sessionDB, $secretKey))): ?>
+
+        <nav>
+
+            <a class="navtab" href='./profile.php'>
+                <?= isLogged($sessionDB, $secretKey)['name'] ?>
+            </a>
+
+            <a class="navtab" href='./logout.php'>
+                Déconnexion
+            </a>
+        </nav>
+
+    <?php else: ?>
+        <nav>
+            <a class="navtab" href='./register.php'>
+                Inscription
+            </a>
+            <a class="navtab" href='./login.php'>
+                Connexion
+            </a>
+        </nav>
+    <?php endif; ?>
+
+
+
 </header>
