d682ce7d77e13c8f832362c4cc68bbd3d8570cf9
Elise Geoffroy <elise.geoffroy03@gmail.com>
2024-10-01 15:57:12 +0200

    error handle

---
+++ error.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel='stylesheet' href="./public/css/general-style.css">
+
+</head>
+
+<body>
+    <header>LilyBlog - Error</header>
+    <a href="./index.php" class=retour>
+        <button id="retour">
+            <img src="./data/retour.jpg" width="30" height="30">
+        </button>
+        <label for="retour">Retour à la page d'accueil</label>
+    </a>
+
+    <h1>Une erreur est survenue</h1>
+    <footer>Lily Creative Commons</footer>
+</body>
+
+</html>
